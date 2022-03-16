81cc4a4b67ab5928c4db4884d5cb4d254a8cc242
MuhamadSahrul <sahrulpugas141@gmail.com>
2022-03-16 17:08:18 +0700

    versi8

--- koneksi.php
+++ koneksi.php
@@ -1,9 +1,9 @@
 <?php
-$conn_str ="host=john.db.elephantsql.com".
-    "port=5432".
-    "user=ijjrarhf".
-    "dbname=ijjrarhf".
-    "password=CBhTyZrQDgFHhNnMbpx-Uzq0W7nsV9uy";
+$conn_str ="host=john.db.elephantsql.com " .
+    "port=5432 ".
+    "user=ijjrarhf ".
+    "dbname=ijjrarhf ".
+    "password=CBhTyZrQDgFHhNnMbpx-Uzq0W7nsV9uy ";
 $conn = pg_connect($conn_str);
 
 if($conn_str){
